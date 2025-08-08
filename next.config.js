1012b3cfa85f1449649f317552e9e9fe8e8f8f41
zippupapp <zippupapp@gmail.com>
2025-08-08 09:57:38 +0200

    next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/(.*)',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
+    ];
+  },
+}
+
+module.exports = nextConfig
