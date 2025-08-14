3fb33bc91e1870fa9e7bf6c7a2afc7d9a8851427
zippupapp <zippupapp@gmail.com>
2025-08-14 22:05:42 +0200

    Update index.js

--- pages/api/promotions/index.js
+++ pages/api/promotions/index.js
@@ -1,5 +1,10 @@
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
   if (req.method === 'GET')  return res.status(200).json([]);
-  if (req.method === 'POST') return res.status(200).json({ ok:true });
-  res.status(405).end();
+  if (req.method === 'POST') return res.status(200).json({ ok: true });
+  return res.status(405).end();
 }
