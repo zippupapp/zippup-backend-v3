5a66ac3dde82383b30e6173aad57b455d2a4fbc0
zippupapp <zippupapp@gmail.com>
2025-08-14 22:03:47 +0200

    Update vendors.js

--- pages/api/food/vendors.js
+++ pages/api/food/vendors.js
@@ -1,4 +1,9 @@
 export default function handler(req, res) {
-  if (req.method === 'GET') return res.status(200).json([]); // later: real vendors
-  res.status(405).end();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
+  if (req.method === 'GET') return res.status(200).json([]);
+  return res.status(405).end();
 }
